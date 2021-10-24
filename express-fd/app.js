1cc39f7a256bd92aa240300297b4777f4c295432
Mourad EL CADI <mouradface1998@gmail.com>
2021-10-24 11:46:46 +0100

    expressjs routes

--- express-fd/app.js
+++ express-fd/app.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const path = require("path");
 
+const { products } = require("./data");
+
 const app = express();
 
 // set up static and middleware
 app.use(express.static("./public"));
 
 // API route
-app.get("/api/", (req, res) => {
-      res.json([{ name: "Mourad" }, { name: "Amondo" }]);
+app.get("/api/products", (req, res) => {
+      res.json(products);
 });
 
+app.get("/api/products/:productId", (req, res) => {
+      let { productId } = req.params;
+
+      if (Number(productId) == NaN) {
+            res.status(404).end("Not Found 404");
+            return;
+      }
+
+      singleProduct = products.find(
+            (product) => product.id === Number(productId)
+      );
+
+      if (!singleProduct) {
+            res.status(404).end("Not Found 404");
+            return;
+      }
+
+      res.json(singleProduct);
+});
+
+// Usual routes
 app.get("/", (req, res) => {
-      // res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
+      res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
 });
 
 app.get("/about", (req, res) => {
---
+++ express-fd/data.js
@@ -0,0 +1,59 @@
+let products = [
+      {
+            id: 0,
+            name: "hero Product",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            hero: "OMG This just came out today!",
+            image: "http://placehold.it/940x300/999/CCC",
+      },
+      {
+            id: 1,
+            name: "Product 1",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            info: "This is the latest and greatest product from Derp corp.",
+            image: "http://placehold.it/300x300/999/CCC",
+      },
+      {
+            id: 2,
+            name: "Product 2",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            offer: "BOGOF",
+            image: "http://placehold.it/300x300/999/CCC",
+      },
+      {
+            id: 3,
+            name: "Product 3",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            image: "http://placehold.it/300x300/999/CCC",
+      },
+      {
+            id: 4,
+            name: "Product 4",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            offer: "No srsly GTFO",
+            image: "http://placehold.it/300x300/999/CCC",
+      },
+      {
+            id: 5,
+            name: "Product 5",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            image: "http://placehold.it/300x300/999/CCC",
+      },
+      {
+            id: 6,
+            name: "Product 6",
+            detail: "Lorem ipsum dolor sit amet",
+            price: "99",
+            info: "This is the latest and greatest product from Derp corp.",
+            offer: "info with offer",
+            image: "http://placehold.it/300x300/999/CCC",
+      },
+];
+
+module.exports = { products };
